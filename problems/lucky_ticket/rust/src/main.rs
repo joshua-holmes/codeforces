3d8a75d0f0aed8fb626478dcaa82426311d26492
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-11-12 15:49:39 -0800

    146A - Lucky Ticket Rust 2021 Accepted 30 ms 0 KB

---
+++ problems/lucky_ticket/rust/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+fn main() -> io::Result<()> {
+    io::stdin().read_line(&mut String::new())?;
+
+    let mut ticket = String::new();
+    io::stdin().read_line(&mut ticket)?;
+    let ticket = ticket.trim();
+
+    let len = ticket.len();
+
+    let ticket_bytes = ticket.as_bytes();
+    let mut sum1 = 0;
+    let mut sum2 = 0;
+    for (i, b) in ticket_bytes.iter().enumerate() {
+        let num = (*b as char).to_digit(10).unwrap();
+        if (num != 4) && (num != 7) {
+            println!("NO");
+            return Ok(());
+        }
+        if i < len / 2 {
+            sum1 += num;
+        } else {
+            sum2 += num;
+        }
+    }
+
+    let output = if sum1 == sum2 { "YES" } else { "NO" };
+    println!("{}", output);
+
+    Ok(())
+}
